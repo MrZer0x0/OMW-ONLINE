d6f5b71ab92b74ade5338b30a6539736eddcf269
MrZer0x0 <91191738+MrZer0x0@users.noreply.github.com>
2025-06-06 04:15:35 +0400

    Update version.sh

--- buildscripts/include/version.sh
+++ buildscripts/include/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-NDK_VERSION="r21e"
-NDK_HASH="ad7ce5467e18d40050dc51b8e7affc3e635c85bd8c59be62de32352328ed467e"
+NDK_VERSION="r27b"
+NDK_HASH="caac638f060347c9aae994e718ba00bb18413498d8e0ad4e12e1482964032997"
 ANDROID_API="21"
 
 # End of configurable options
@@ -9,32 +9,35 @@ ANDROID_API="21"
 NDK_FILE="ndk-$NDK_VERSION.zip"
 
 if [[ $ARCH = "arm" ]]; then
-	ABI="armeabi-v7a"
-	NDK_TRIPLET="arm-linux-androideabi"
-	FFMPEG_CPU="armv7-a"
-	BOOST_ARCH="arm"
-	BOOST_ADDRESS_MODEL="32"
-	ASAN_ARCH="arm"
+    ABI="armeabi-v7a"
+    NDK_TRIPLET="arm-linux-androideabi" 
+    FFMPEG_CPU="armv7-a"
+    BOOST_ARCH="arm"
+    BOOST_ADDRESS_MODEL="32"
+    LUAJIT_HOST_CC="gcc -m32"
+    ASAN_ARCH="arm"
 elif [[ $ARCH = "arm64" ]]; then
-	ABI="arm64-v8a"
-	NDK_TRIPLET="aarch64-linux-android"
-	FFMPEG_CPU="armv8-a"
-	BOOST_ARCH="arm"
-	BOOST_ADDRESS_MODEL="64"
-	ASAN_ARCH="aarch64"
+    ABI="arm64-v8a"
+    NDK_TRIPLET="aarch64-linux-android"
+    FFMPEG_CPU="armv8-a"
+    BOOST_ARCH="arm"
+    BOOST_ADDRESS_MODEL="64"
+    LUAJIT_HOST_CC="gcc -m64"
+    ASAN_ARCH="aarch64"
 elif [[ $ARCH = "x86_64" ]]; then
-	ABI="x86_64"
-	NDK_TRIPLET="x86_64-linux-android"
-	FFMPEG_CPU="intel"
-	BOOST_ARCH="x86"
-	BOOST_ADDRESS_MODEL="64"
+    ABI="x86_64"
+    NDK_TRIPLET="x86_64-linux-android"
+    FFMPEG_CPU="intel"
+    BOOST_ARCH="x86"
+    BOOST_ADDRESS_MODEL="64"
+    LUAJIT_HOST_CC="gcc -m64"
 elif [[ $ARCH = "x86" ]]; then
-	ABI="x86"
-	NDK_TRIPLET="i686-linux-android"
-	FFMPEG_CPU="intel"
-	BOOST_ARCH="x86"
-	BOOST_ADDRESS_MODEL="32"
+    ABI="x86"
+    NDK_TRIPLET="i686-linux-android"
+    FFMPEG_CPU="intel"
+    BOOST_ARCH="x86"
+    BOOST_ADDRESS_MODEL="32"
+    LUAJIT_HOST_CC="gcc -m32"
 else
-	echo "Unknown architecture: $ARCH"
-	exit 1
-fi
+    echo "Unknown architecture: $ARCH"
+    exit 1
