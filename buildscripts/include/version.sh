552b32497ef71c6738d2240faca07ed50a3bc9fd
MrZer0x0 <91191738+MrZer0x0@users.noreply.github.com>
2025-06-06 04:21:00 +0400

    Update version.sh

--- buildscripts/include/version.sh
+++ buildscripts/include/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 NDK_VERSION="r27b"
-NDK_HASH="caac638f060347c9aae994e718ba00bb18413498d8e0ad4e12e1482964032997"
+NDK_HASH="ab9dbab873fff677deb2cfd95ea60b9295ebd53b58ec8533e9e1110b2451e540"
 ANDROID_API="21"
 
 # End of configurable options
